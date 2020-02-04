30919bd23b1e8d6a4d926c783d9fd9737ad837ce
Yvette Clapp <ymclapp@yahoo.com>
2020-02-04 12:50:36 -0600

    guess number is working and updated CSS to get boxes to work better

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 // use strict is almost alwayst the first line of our js files
 console.log('It is working!');
+var user="";
 
 function firstQuestion() {
-	var user = prompt("What is your name?");
+	user = prompt("What is your name?");
 	// console.log(user + " , thank you for taking the time to visit my page!");
 	alert(user + " , thank you for taking the time to visit my page!");
 
@@ -69,53 +70,79 @@ function questionThree() {
 	}
 }
 
+// function questionFour() {
+//     var guessNumber = prompt("I have a number in my bit-brain that is between 1 and 23.  What number am I thinking of?");
+//     // // console.log(guessNumber)
+//     if (guessNumber === '17') {
+//         alert("You got it!");
+//     } else if (guessNumber < '16' || guessNumber > '18') {
+//         alert("You're too cold!");
+//     } else if (guessNumber === '16' || guessNumber === '18') {
+//         alert("You're getting warmer!");
+//     } else {
+//         alert("Please provide an answer in the correct format/between 1 and 23");
+//     }
+// }
+
 function questionFour() {
-    var guessNumber = prompt("I have a number in my bit-brain that is between 1 and 23.  What number am I thinking of?");
-    // // console.log(guessNumber)
-    if (guessNumber === '17') {
-        alert("You got it!");
-    } else if (guessNumber < '16' || guessNumber > '18') {
-        alert("You're too cold!");
-    } else if (guessNumber === '16' || guessNumber === '18') {
-        alert("You're getting warmer!");
-    } else {
-        alert("Please provide an answer in the correct format/between 1 and 23");
-    }
+    var userCorrect = false;
+    var guessNumber = 17;
+    var guesses = 0;
+	while (guesses < 6) {
+        var guessNumber = prompt("I have a number in my bit-brain that is between 1 and 23.  What number am I thinking of?");
+
+	for (var i = 0; i < 6; i++) {
+		if (guessNumber === '17') {
+			alert("You got it!");
+			guesses = 6;
+			userCorrect = true;
+			break;
+		}
+	}
+		if (guesses !== 6 && guessNumber < '16' || guessNumber > '18') {
+			alert("You're too cold!");
+		}
+
+		guesses++;
+
+		if (guesses !== 6 && guessNumber === '16' || guessNumber === '18') {
+			alert("You're getting warmer!");
+	}
+		guesses++;
+
+	} 	if (guesses = 6) {
+			alert("The correct answer is 17.  Thank you for playing!");
+	}
 }
 
 function lastQuestion() {
     var userCorrect = false;
     var bands = ['bon jovi', 'guns and roses', 'tesla', 'motley crue', 'cinderella', 'kix', 'def leppard', 'poison', 'warrant'];
     var guesses = 0;
-    while (guesses < 7) {
+	while (guesses < 6) {
         var eighties = prompt("In the 80s, I loved hair bands.  Name a hair band and I will let you know if it was one that I liked.  Go!").toLowerCase();
 
-        for (var i = 0; i < bands.length; i++) {
-            if (eighties === bands[i]) {
-                // 	 console.log('You guessed right.');
-                alert("Bang your head!  Way to go!\n\nHere are the bands that were my top picks:  Bon Jovi, Guns and Roses, Tesla, Motley Crue, Cinderella, Kix, Def Leppard, Warrant, and Poison.");
-                alert(user + ", thank you again for visiting my page!");
-                guesses = 7;
-                userCorrect = true;
-                break;
-            }
-            if (guesses !== 7) {
-                alert(user + ' ,dude, try again!');
-            }
-            guesses++;
-        }
-
-	if (userCorrect === false) {
-		alert("Keep practicing your cowbell!\n\nHere are the bands that were my top picks:  Bon Jovi, Guns and Roses, Tesla, Motley Crue, Cinderella, Kix, Def Leppard, Warrant, and Poison.");
-		alert(user + ", thank you again for visiting my page!");
+	for (var i = 0; i < bands.length; i++) {
+		if (eighties === bands[i]) {
+			alert("Bang your head!  Way to go!\n\nHere are the bands that were my top picks:  Bon Jovi, Guns and Roses, Tesla, Motley Crue, Cinderella, Kix, Def Leppard, Warrant, and Poison.");
+			alert(user + ", thank you again for visiting my page!");
+			guesses = 6;
+			userCorrect = true;
+			break;
+		}
+	}	
+		if (guesses !== 6) {
+            alert(user + ' ,dude, try again!');
+		}
+		guesses++;
+	}	
+		if (userCorrect === false) {
+			alert("Keep practicing your cowbell!\n\nHere are the bands that were my top picks:  Bon Jovi, Guns and Roses, Tesla, Motley Crue, Cinderella, Kix, Def Leppard, Warrant, and Poison.");
+			alert(user + ", thank you again for visiting my page!");
+		}
 	}
-}
 
-
-
-
-
-
+	// Add in the info from Justin's that added up the numbers or Craig's video
 
 firstQuestion();
 dogNumber();
@@ -124,4 +151,3 @@ questionTwo();
 questionThree();
 questionFour();
 lastQuestion();
-}
